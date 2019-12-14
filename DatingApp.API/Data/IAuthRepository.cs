1111dbf11c5f8aa84bfea42b23f4d44c3a217d93
Yossi Levi <yosefor888@gmail.com>
2019-12-14 21:03:32 +0200

    aa

--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -1,15 +1,11 @@
-﻿using DatingApp.API.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
 {
-   public interface IAuthRepository
+    interface IAuthRepository
     {
-        Task<User> Register(User user, string password);
-        Task<User> LogIn(string userName, string password);
-        Task<bool> UserExist(string userName);
     }
 }
