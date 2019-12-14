828064e72948bd53a7fd6f23bedb4df569623fae
Yossi Levi <yosefor888@gmail.com>
2019-12-14 20:54:04 +0200

    add user model

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<User> LogIn(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExist(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using DatingApp.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
 {
-    interface IAuthRepository
+   public interface IAuthRepository
     {
+        Task<User> Register(User user, string password);
+        Task<User> LogIn(string userName, string password);
+        Task<bool> UserExist(string userName);
     }
 }
